7e6f7e02c21b2c855f0e5fcdc3065943cb83bdd5
abbott <1041976642@qq.com>
2022-03-16 21:57:29 +0800

    testFile

--- src/file/TestFile.java
+++ src/file/TestFile.java
@@ -1,5 +1,9 @@
 package file;
 
-public class TestFile {
+import java.io.File;
 
+public class TestFile {
+	public static void main(String[] args){
+		File f1	= new File("E:/project/test.txt");
+	}
 }
