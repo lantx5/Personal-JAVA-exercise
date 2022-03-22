536b26a0680c3e7871128066008c208ee9452db5
DELL <lantianxiang1997@outlook.com>
2022-03-22 10:54:19 +0800

    java

---
+++ src/stream/StreamPracties3.java
@@ -0,0 +1,15 @@
+package stream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
+public class StreamPracties3 {
+	public static void main() {
+		
+	}
+}
